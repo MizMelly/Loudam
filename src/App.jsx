c4ee8eb853df054e22893c7a0f6ec1cdc6331a41
MizMelly <jazminelike@yahoo.com>
2026-04-23 13:07:11 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import ForgotPassword from './pages/auth/ForgotPassword';
 import Dashboard from './pages/dashboard/Dashboard';
 import FileComplaint from './pages/complaint/FileComplaint';
 import TrackComplaints from './pages/complaint/TrackComplaints';
-import AdminDashboard from './pages/admin/admindashboard';
+import AdminDashboard from './pages/admin/Admindashboard';
 import Brands from './components/brands/brands';
 
 const ProtectedRoute = ({ children, isAuthenticated }) => {
